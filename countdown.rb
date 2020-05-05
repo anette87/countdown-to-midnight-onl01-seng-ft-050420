df07e86b63c02081ca32f6979e7bad038cb59fb7
Anette Rivera <anettem.rivera@gmail.com>
2020-05-05 19:07:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,13 +3,18 @@
 def countdown(seconds)
   while seconds > 0 
   puts "#{seconds} SECOND(S)!"
-  sleep 0.75
   seconds -= 1
 end
   "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(seconds)
-  
+  while seconds > 0 
+  puts "#{seconds} SECOND(S)!"
+  sleep 1
+  seconds -= 1
+end
+  "HAPPY NEW YEAR!"
+end
 
 puts countdown(12)
